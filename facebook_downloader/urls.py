c65fcb1956a3c3412734d281d7bfa14800d29e29
Thiago Garcia <thiagogds14@gmail.com>
2016-10-04 14:38:01 -0300

    Simple view to download data from facebook
    This view simple uses a token in the env to make a query to facebook and show it as json

--- facebook_downloader/urls.py
+++ facebook_downloader/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from gate_id import urls as gate_id_urls
+from core import urls as core_urls
 
 urlpatterns = [
     url(r'', include(gate_id_urls, namespace='gate_id')),
+    url(r'', include(core_urls, namespace='core')),
     url(r'^admin/', include(admin.site.urls)),
 ]
